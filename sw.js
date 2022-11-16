4b4a7d4825f2d9382ce5c63a25608689e4fe973b
Dipesh161 <dipesh.verma@tallysolutions.com>
2022-11-16 14:36:13 +0530

    fix

--- sw.js
+++ sw.js
@@ -1,14 +1,17 @@
 self.addEventListener('push', function(e){
     var  options ={
         body:'this is test meaasage',
+        icon:'image/example.png',
         vibrate:[100,50,100],
         data:{
             dateOfArrival: Date.now(),
             primaryKey: '2'
         },
         actions: [
-            {action: 'explore', title:'explore this world'},
-            {action: 'close', title: 'close'},
+            {action: 'explore', title:'explore this world',
+              icon:'image/example.png'},
+            {action: 'close', title: 'close',
+              icon:'image/example.png'},
         ]
     };
     e.waitUntil(
