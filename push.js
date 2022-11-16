25a543f0e61b14c5d7caf402b62ed1b5a78516b7
Dipesh161 <dipesh.verma@tallysolutions.com>
2022-11-16 14:03:04 +0530

    fix

--- push.js
+++ push.js
@@ -1,5 +1,18 @@
 var push = require('web-push');
 
-let vapidKeys = push.generateVAPIDKeys();
+let vapidKeys = {
+    publicKey: 'BFa69bGrT5I5-TSR-J-yxAtLFGAdA18uSVettZNGIJayCWGq3oLRCpAoUK1Qce-a3GGtVCyaJyR4RTOH8ve-iy4',
+    privateKey: 'Cjw0xfLyFpwy0ktB0CBS2FisYOpdVddzrrvBZN3OhTY'
+  }
 
-console.log(vapidKeys);
+  push.setVapidDetails('mailto:test@code.com',vapidKeys.publicKey,vapidKeys.privateKey)
+
+  let sub={endpoint:"https://fcm.googleapis.com/fcm/send/e85YprdTOPM:APA91bHjRxP-5UHGJSdMInn8K0lVOauxMb_EfprGoCkeGZTL-2W1MlHqpgyUR6K3jg1tiQePrgwVNKr99r4ZShK1OVuK1XZXF-yfZXPoFue1uRTGzSX3MKx7-3HOWs7h3iPkJCLEeYD3",
+  expirationTime:null,
+  keys:{
+    p256dh:"BGRiiu8XeWM6hHYL6hNn6xkVAvX17P50QdVlME1pJg-ceZT_2BOGJRu5phfbW5F3_0-hkUVxKmID7CmWmaHxF64",
+    auth:"PfMtEfAL8Q8WinbOsihulg"
+    }
+};
+
+  push.sendNotification(sub,'test message')
---
+++ sw.js
@@ -0,0 +1,17 @@
+self.addEventListener('push', function(e){
+    var  options ={
+        body:'this is test meaasage',
+        vibrate:[100,50,100],
+        data:{
+            dateOfArrival: Date.now(),
+            primaryKey: '2'
+        },
+        actions: [
+            {action: 'explore', title:'explore this world',}
+            {action: 'close', title: 'close'}
+        ]
+    };
+    e.waitUntil(
+    self.ServiceWorkerRegistration.showNotification('hello world',options)
+    );
+});
