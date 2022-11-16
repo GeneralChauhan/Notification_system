b44bf6195b360398447f3c011b323c84d74fa125
Dipesh161 <dipesh.verma@tallysolutions.com>
2022-11-16 14:45:59 +0530

    fix

--- sw.js
+++ sw.js
@@ -15,6 +15,6 @@ self.addEventListener('push', function(e){
         ]
     };
     e.waitUntil(
-    self.registration.showNotification('hello world',options)
+    self.ServiceWorkerRegistration.showNotification('hello world',options)
     );
 });
