c0963edf9a03e92127977f9a5549c58a08fd9b28
God <abhinabpratap1311@gmail.com>
2022-11-22 16:13:57 +0530

    Temp

--- push.js
+++ push.js
@@ -17,15 +17,8 @@ push.setVapidDetails(
   vapidKeys.privateKey
   );
 
-let sub={
-  endpoint:"https://fcm.googleapis.com/fcm/send/e85YprdTOPM:APA91bHjRxP-5UHGJSdMInn8K0lVOauxMb_EfprGoCkeGZTL-2W1MlHqpgyUR6K3jg1tiQePrgwVNKr99r4ZShK1OVuK1XZXF-yfZXPoFue1uRTGzSX3MKx7-3HOWs7h3iPkJCLEeYD3",
-expirationTime: null,
-keys:{
-  p256dh:"BGRiiu8XeWM6hHYL6hNn6xkVAvX17P50QdVlME1pJg-ceZT_2BOGJRu5phfbW5F3_0-hkUVxKmID7CmWmaHxF64",
-  auth:"PfMtEfAL8Q8WinbOsihulg"
-  }
-};
+let sub={"endpoint":"https://wns2-pn1p.notify.windows.com/w/?token=BQYAAABO%2fK72yR1i79YMFnlOl5FylbJT%2fLAvOhjuxV0rpeSC82DuKSltApDMrFw%2fTQUFNrV0AJ8XPH6%2b1m7SQqGHrpDIzQfB6V4Tx%2fupNX3ascg%2fzgOerA3Lfe954joO5QpVPAfYdne%2bLeGQCJBMCdSJvOWpKc3MKwBGgSI5UIu9Q4L1iHL379GNki3fc%2bKTO0yLu7S2c%2bcDKSBVppSz9H7UbLChnj5u7nNWHGPvWXMJ57%2b9JW77xW4xdZNsABHjfpiBr9guIr%2banz2K6gyvy5ExB8lQlBaB5p6aWeKRhbfl%2fnxv4nnXMg0%2fOpKw%2bb%2b0s%2f8aB3%2bgBnam7QUF87WYwYGsieqW4SAc4pgqbzm1tqAJbPe3ZA%3d%3d","expirationTime":null,"keys":{"p256dh":"BGpZphIcgmDno0Sa3KhpRWguyUwL3EL28GZLX9yQoG9VApr6puzwI7tikEZs_kvNE_dDli7bujYZM_MQCW_w2fQ","auth":"xLu2ez9nI2hVGsjJGmvY8Q"}}
 
 push.sendNotification(sub,'test message');
 
-console.log(vapidKeys);
+// console.log(vapidKeys);
